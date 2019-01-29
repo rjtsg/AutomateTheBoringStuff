b7f2f9d668ed6ba5adc68323f77a6f38dfb9b5f4
rjtsg <rjtsg@users.noreply.github.com>
2019-01-29 14:57:34 +0100

    Character Count

---
+++ characterCount.py
@@ -0,0 +1,8 @@
+message = 'It was a bright cold day in April, and the clocks were striking thirtheen.'
+count = {}
+
+for character in message:
+    count.setdefault(character,0)
+    count[character] = count[character] +1
+
+print(count)
---
+++ prettyCharacterCount.py
@@ -0,0 +1,10 @@
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirtheen.'
+count = {}
+
+for character in message:
+    count.setdefault(character,0)
+    count[character] = count[character] +1
+
+pprint.pprint(count)
