ed61765fe5e776d5bbc65369cadf0e94d63ef360
rjtsg <rjtsg@users.noreply.github.com>
2019-01-29 20:16:01 +0100

    xxx

---
+++ catnapping.py
@@ -0,0 +1,11 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+"""This is a test Programm
+Written by Roy
+This program was designed for Python 3, not Python 2.
+"""
